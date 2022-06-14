f15afe28dbe301b5f281bb868e12b1e5a515315b
Long <88829179+longdp121@users.noreply.github.com>
2022-06-14 19:35:23 +0700

    commit exam

--- web_advance/b8_jquery/main.js
+++ web_advance/b8_jquery/main.js
@@ -1,20 +1,20 @@
-let inputScore = $("#score")
-let btn = $("#btn")
-let resultTag = $("#result")
+let inputScore = $("#score");
+let btn = $("#btn");
+let resultTag = $("#result");
 
-btn.on("click", function() {
-    let score = inputScore.val();
-    if (score >=6 && score <= 8) {
-        resultTag.html(
-            `<p style="color: green">Gioi</p>`
-        )
-    } else if (score >= 4 && score < 6) {
-        resultTag.html(
-            `<p style="color: yellow">Dat</p>`
-        )
-    } else if (score < 4) {
-        resultTag.html(
-            `<p style="color: red">Khong dat</p>`
-        )
-    }
-})
+btn.on("click", function () {
+  let score = inputScore.val();
+  let result = "";
+  let color = "";
+  if (score >= 6 && score <= 8) {
+    result = "Gioi";
+    color = "green"
+  } else if (score >= 4 && score < 6) {
+    result = "Dat";
+    color = "yellow"
+  } else if (score < 4) {
+    result = "Khong dat";
+    color = "red"
+  }
+  resultTag.html(`<p style="color: ${color}">${result}</p>`)
+});
