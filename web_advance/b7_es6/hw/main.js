d0613431b273bb10270965abb25bad721e18e179
Long <88829179+longdp121@users.noreply.github.com>
2022-06-16 11:01:46 +0700

    redo hw

--- web_advance/b7_es6/hw/main.js
+++
@@ -1,9 +0,0 @@
-let profestionOptions = document.getElementById("profestion-filters")
-
-profestionOptions.onfocus = function() {
-    console.log(10)
-}
-
-// profestionOptions.onclick = function() {
-//     console.log(10)
-// }
