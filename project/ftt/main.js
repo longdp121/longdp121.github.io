15ddb180513a85ea67a804dfbf3d82e3d6cb8700
Long <88829179+longdp121@users.noreply.github.com>
2022-07-02 19:46:43 +0700

    commit ftt

--- project/ftt/main.js
+++ project/ftt/main.js
@@ -1,17 +1,43 @@
+let tours = [
+    {img: "img/tours/tour_thumb_1.jpg", name: "Food tour 1", minPrice: 25, duration: 3},
+    {img: "img/tours/tour_thumb_2.jpg", name: "Food tour 2", minPrice: 25, duration: 3},
+    {img: "img/tours/tour_thumb_3.jpg", name: "Food tour 3", minPrice: 25, duration: 1},
+    {img: "img/tours/tour_thumb_4.jpg", name: "Food tour 4", minPrice: 2, duration: 3},
+    {img: "img/tours/tour_thumb_5.jpg", name: "Food tour 5", minPrice: 5, duration: 30},
+    {img: "img/tours/tour_thumb_6.jpg", name: "Food tour 6", minPrice: 215, duration: 3},
+    {img: "img/tours/tour_thumb_1.jpg", name: "Food tour 7", minPrice: 255, duration: 9},
+    {img: "img/tours/tour_thumb_2.jpg", name: "Food tour 8", minPrice: 25, duration: 6.8},
+    {img: "img/tours/tour_thumb_3.jpg", name: "Food tour 9", minPrice: 25, duration: 6.8},
+];
+
 let main = document.getElementById("main");
 let tour = document.getElementById("tour");
-let homeBtn = document.getElementById("home-btn");
+let rowTours = document.getElementById("row-tours");
 
-function testBtn() {
-    main.className = "container-fluid display-none";
-    tour.className = "container-fluid";
+function renderTours (arr) {
+    let html = "";
+    for (obj of arr) {
+        html += `
+        <div class="card border-0 px-4 py-4 col-lg-4 col-md-4 col-sm-6 col-sx-12">
+        <div class="card-body-box">
+          <img src=${obj.img} alt="tour-thumb" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${obj.name}</h5>
+            <div class="row">
+              <div class="col-6">
+                <p><b>From: </b>$${obj.minPrice}/person</p>
+              </div>
+              <div class="col-6">
+                <p><b>Duration: </b>${obj.duration}-hours</p>
+              </div>
+            </div>
+            <button type="button" class="btn btn-success" onclick="testBtn()">Bấm đi</button>
+          </div>
+        </div>
+      </div>
+        `;
+    }
+    rowTours.innerHTML = html;
 }
 
-function renderHomePage() {
-    console.log("This is main classname before: ", main.className);
-    console.log("This is tour classname before: ",tour.className)
-    main.className = "container-fluid";
-    tour.className = "container-fluid display-none";
-    console.log(main.className);
-    console.log(tour.className)
-}
+renderTours(tours)
