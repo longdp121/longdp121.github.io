c819595d7e3ee8c3b4cdfda839b9907b43aabb3f
Long <88829179+longdp121@users.noreply.github.com>
2022-07-05 18:55:50 +0700

    commit ftt

--- project/ftt/main.js
+++ project/ftt/main.js
@@ -1,46 +1,74 @@
+// let tours = [
+//     {tour_id: "ft1", imgThumb: "./img/tours/FoodTour1/tour_imgs/img1.jpg", name: "Food tour 1", minPrice: 25, duration: 3},
+//     {tour_id: "ft2", imgThumb: "./img/tours/FoodTour1/tour_imgs/img2.jpg", name: "Food tour 2", minPrice: 25, duration: 3},
+//     {tour_id: "ft3", imgThumb: "./img/tours/FoodTour1/tour_imgs/img3.jpg", name: "Food tour 3", minPrice: 25, duration: 1},
+//     {tour_id: "ft4", imgThumb: "./img/tours/FoodTour1/tour_imgs/img4.jpg", name: "Food tour 4", minPrice: 2, duration: 3},
+//     {tour_id: "ft5", imgThumb: "./img/tours/FoodTour1/tour_imgs/img5.jpg", name: "Food tour 5", minPrice: 5, duration: 3},
+// ];
+
 let tours = [
-    {tour_id: 1, img: "img/tours/tour_thumb_1.jpg", name: "Food tour 1", minPrice: 25, duration: 3},
-    {tour_id: 2, img: "img/tours/tour_thumb_2.jpg", name: "Food tour 2", minPrice: 25, duration: 3},
-    {tour_id: 3, img: "img/tours/tour_thumb_3.jpg", name: "Food tour 3", minPrice: 25, duration: 1},
-    {tour_id: 4, img: "img/tours/tour_thumb_4.jpg", name: "Food tour 4", minPrice: 2, duration: 3},
-    {tour_id: 5, img: "img/tours/tour_thumb_5.jpg", name: "Food tour 5", minPrice: 5, duration: 3},
-    {tour_id: 6, img: "img/tours/tour_thumb_6.jpg", name: "Food tour 6", minPrice: 21, duration: 3},
-    {tour_id: 7, img: "img/tours/tour_thumb_1.jpg", name: "Food tour 7", minPrice: 25, duration: 9},
-    {tour_id: 8, img: "img/tours/tour_thumb_2.jpg", name: "Food tour 8", minPrice: 25, duration: 6},
-    {tour_id: 9, img: "img/tours/tour_thumb_3.jpg", name: "Food tour 9", minPrice: 25, duration: 6},
-];
+  {
+    tour_id: "FT",
+    name: "Hanoi Street Food Tours",
+    imgThumb: "./img/tours/FoodTour1/tour_thumb.jpg",
+    img: [
+      "img/tours/FoodTour1/tour_imgs/img1.jpg",
+      "img/tours/FoodTour1/tour_imgs/img2.jpg",
+      "img/tours/FoodTour1/tour_imgs/img3.jpg",
+      "img/tours/FoodTour1/tour_imgs/img4.jpg"
+    ],
+    minPrice: 25,
+    duration: 3
+  }
+]
 
-let main = document.getElementById("main");
-let tour = document.getElementById("tour");
-let rowTours = document.getElementById("row-tours");
-let tourImgThumb = document.getElementById("tour-img-thumb");
+/////////// Creat function
 
-// Creat func
+//###### App function
 function getTourById(id, arr) {
-  return arr.find(function(obj) {
-    return obj.tour_id == id;
+  let tour = arr.find(function (obj) {
+    return obj.tour_id === id;
   })
+  return tour
 }
 
-function rederNavDropdown (arr) {
-  let navDropdownTour = document.getElementById("navbar-tour-list");
+function renderTourfromTourObj(obj) {
+  let tourImgs = document.getElementById("tourImgs");
+  console.log(tourImgs)
+  let imgs = obj.img;
   let html = "";
-  arr.forEach(function(obj) {
-    html += `
-    <li><button type="button" class="dropdown-item" id="tour_id_${obj.tour_id}" onclick="viewTourNavBtn()">${obj.name}</button></li>
-    `
+  imgs.forEach(function(img) {
+    html += `<img src=${img} alt="tour_imgs">`
   })
-  console.log(html)
-  navDropdownTour.innerHTML = html;
+  tourImgs.innerHTML = html;
+  let tourDetails = document.getElementById("tourDetails");
+  // tourDetails.innerHTML = `
+  // <h1>${obj.name}</h1>
+  // <p>From: $${obj.minPrice}/person</p>
+  // <p>${obj.duration}-hours</p>
+  // `;
 }
 
-function renderTours (arr) {
-    let html = "";
-    for (obj of arr) {
-        html += `
-        <div class="card border-0 px-4 py-4 col-lg-4 col-md-4 col-sm-6 col-sx-12">
+// renderTourfromTourObj(tour)
+
+//###### Renderers
+function renderTourNavs(arr) {
+  let navbarDropdownTours = document.getElementById("navbarDropdownTours").nextElementSibling;
+  let html = "";
+  arr.forEach(function (obj) {
+    html += `<li><a class="dropdown-item" href="#" id="navbarDropdownTours_tourId.${obj.tour_id}" onclick="viewTourBtn()">${obj.name}</a></li>`
+  })
+  navbarDropdownTours.innerHTML = html
+}
+
+function renderTourCards(arr) {
+  let rowTours = document.getElementById("row-tours");
+  let html = "";
+  arr.forEach(function (obj) {
+    html += `
+      <div class="card border-0 px-4 py-4 col-lg-4 col-md-4 col-sm-6 col-sx-12" id="tourCard_tourId.${obj.tour_id}">
         <div class="card-body-box">
-          <img src=${obj.img} alt="tour-thumb" class="card-img-top">
+          <img src=${obj.imgThumb} alt="tour-thumb" class="card-img-top">
           <div class="card-body">
             <h5 class="card-title">${obj.name}</h5>
             <div class="row">
@@ -51,70 +79,33 @@ function renderTours (arr) {
                 <p><b>Duration: </b>${obj.duration}-hours</p>
               </div>
             </div>
-            <button type="button" class="btn btn-success" id="show-tour-btn-id${obj.tour_id}"" onclick="viewTourBtn()">View Tour</button>
+            <button type="button" class="btn btn-success" id="viewTourBtn_tourId.${obj.tour_id}" onclick="viewTourBtn()">View Tour</button>
           </div>
         </div>
-      </div>
-        `;
-    }
-    rowTours.innerHTML = html;
+      </div>    
+    `
+  });
+  rowTours.innerHTML = html;
 }
-// /////////////////////////////////////////////////////////
 
-rederNavDropdown(tours)
-renderTours(tours)
-
-function viewTourNavBtn(arr) {
-  let tourClickedId = event.target.id.slice(8)
-  console.log(tourClickedId)
-  let tour = getTourById(tourClickedId, tours);
-  console.log(tour)
-  let tourId = document.getElementById("tour");
-  let tourImgThumb = document.getElementById("tour-img-thumb")
-  let tourDetails = document.getElementById("tour-details");
+//###### Buttons
+function viewTourBtn() {
+  let tourClickedId = event.target.id.slice(-2)
+  let main = document.getElementById("main");
+  let tour = document.getElementById("tour");
   main.className = "container-fluid display-none";
-  tourId.className = "container-fluid";
-  let html = "";
-  let img = "";
-  html += `
-    <h1>${tour.name}</h1>
-    <p>From: $${tour.minPrice}/person</p>
-    <p>${tour.duration} hours tour long.</p>
-    `;
-    img += `
-    <img src=${tour.img} alt="tour-img">
-    `;
-    tourDetails.innerHTML = html;
-    tourImgThumb.innerHTML = img;
+  tour.className = "container-fluid";
+  let tourClicked = getTourById(tourClickedId, tours);
+  renderTourfromTourObj(tourClicked)
 }
 
-function viewTourBtn () {
-    let tourId = document.getElementById("tour");
-    let tourImgThumb = document.getElementById("tour-img-thumb")
-    let tourDetails = document.getElementById("tour-details");
-    main.className = "container-fluid display-none";
-    tourId.className = "container-fluid";
-    let tourClickedId = event.target.id.slice(16);
-    let html = "";
-    let img = "";
-    // let tour = tours.find(function(tour) {
-    //     return tour.tour_id == tourClickedId;
-    // })
-    let tour = getTourById(tourClickedId, tours);
-    console.log(tour);
-    html += `
-    <h1>${tour.name}</h1>
-    <p>From: $${tour.minPrice}/person</p>
-    <p>${tour.duration} hours tour long.</p>
-    `;
-    img += `
-    <img src=${tour.img} alt="tour-img">
-    `;
-    tourDetails.innerHTML = html;
-    tourImgThumb.innerHTML = img;
+function showHomeBtn() {
+  let main = document.getElementById("main");
+  let tour = document.getElementById("tour");
+  main.className = "container-fluid";
+  tour.className = "container-fluid display-none";
 }
 
-function showHomePage() {
-    main.className = "container-fluid";
-    tour.className = "container-fluid display-none";
-}
+// /////// Run function
+renderTourCards(tours)
+renderTourNavs(tours)
